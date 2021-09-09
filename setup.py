8a63ab5d65ce16e43090daa231766b2b8f25d82e
Adam Dyess <adyess@gmail.com>
2021-09-08 21:34:47 -0500

    passes simple test

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='captionator',
+      version='1.0',
+      description='Web-service to display captions from uploaded images',
+      author='Adam Dyess',
+      author_email='addyess@gmail.com',
+      url='https://github.com/addyess/captionator',
+      packages=['captionator'],
+     )
