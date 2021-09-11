b085f1434d6ea169a47660d87f9accc375b6ca04
Adam Dyess <adyess@gmail.com>
2021-09-11 00:00:50 -0500

    created an almost working update form

--- captionator/db.py
+++ captionator/db.py
@@ -1,49 +1,55 @@
 import mysql.connector
 from collections import namedtuple, OrderedDict
 
-CaptionFile = namedtuple('CaptionFile', 'uuid, name, text')
-
 
 class DB:
+    schema = OrderedDict(
+        id="INT NOT NULL AUTO_INCREMENT PRIMARY KEY",
+        name="VARCHAR(255)",
+        text="TEXT",
+        location="Undefined"
+    )
+
     def __init__(self, config):
         self._config = config
-        print(config)
         self._mydb = mysql.connector.connect(
             host=self._config.mysql_host,
             user=self._config.mysql_user,
             password=self._config.mysql_pass,
             database="captionator"
         )
-        schema = OrderedDict(
-            id="INT NOT NULL AUTO_INCREMENT PRIMARY KEY",
-            name="VARCHAR(255)",
-            text="TEXT"
-        )
-        self._create_table("captions", schema)
 
-    def captions(self, views=None, filters=None):
-        allowed_columns = ['id', 'name', 'text']
+    def captions(self, **kwargs):
+        allowed_columns = self.schema.keys()
+        returned_columns, fetched = self._get('captions', allowed_columns, **kwargs)
         return [
-            CaptionFile(*r) for r in self._get_table(allowed_columns, views, filters)
+            {returned_columns[i]: v for i, v in enumerate(r)}
+            for r in fetched
         ]
 
-    def _get_table(self, table, allowed_columns, views=None, filters=None):
-        cursor = self._mydb.cursor()
+    def set_captions(self, rid, values):
+        valid_to_set = set(self.schema.keys()) - {"id"}
+        settable = {k: values.get(k) for k in valid_to_set if values.get(k)}
+        if settable:
+            self._set("captions", rid, **settable)
+
+    def _get(self, table, allowed_columns, views=None, filters=None):
         select, where = "*", ""
         if views and all(v.lower() in allowed_columns for v in views):
-            select = ",".join(views)
-        if filters and all(v.lower in filters for v in views):
-            where = "where " + " ".join(
-                f"{col} = %({col})s'"
-                for col, value in filters.items()
+            select = ", ".join(views)
+        else:
+            views = list(allowed_columns)
+        if filters and all(f.lower() in allowed_columns for f in filters):
+            where = " WHERE " + " AND ".join(
+                f"{col} = %({col})s"
+                for col in filters
             )
-        cursor.execute(f"SELECT {select} from captions {where}", filters)
-        return cursor.fetchall()
-
-    def _create_table(self, name, schema):
         cursor = self._mydb.cursor()
-        definition = ", ".join(f"{col} {type}" for col, type in schema.items())
-        try:
-            cursor.execute(f"CREATE table {name} ({definition})")
-        except mysql.connector.errors.ProgrammingError:
-            pass
+        statement = f"SELECT {select} FROM {table}{where}"
+        cursor.execute(statement, params=filters)
+        return views, cursor.fetchall()
+
+    def _set(self, table, rid, **settable):
+        settings = "SET " + ", ".join(f"{col} = %({col})s" for col in settable)
+        cursor = self._mydb.cursor()
+        cursor.execute(f"UPDATE {table} {settings} where id = {rid}", settable)
--- captionator/server.py
+++ captionator/server.py
@@ -1,7 +1,9 @@
+import json
 import pathlib
 from bottle import Bottle, run, request, redirect, response
 from bottle import jinja2_view as view
 from bottle import TEMPLATE_PATH
+
 
 TEMPLATE_PATH.insert(0, pathlib.Path(__file__).parent / "views")
 
@@ -15,21 +17,25 @@ class WebUX:
 
     def _route(self):
         self._app.route('/', method='GET', callback=self._root)
-        self._app.route('/update', method='POST', callback=self._update)
+        self._app.route('/captions.json', method='GET', callback=self._captions)
+        self._app.route('/update/<id:int>', method='GET', callback=self._update)
+        self._app.route('/update/<id:int>', method='POST', callback=self._update)
 
     @view('root.j2')
     def _root(self):
-        return dict(
-            available_captions=self._db.captions()
-        )
+        return dict()
 
-    def _update(self):
-        try:
-            user_req = request.json
-        except:
-            response.status = 400
-            return
-        return {}
+    def _captions(self):
+        return json.dumps(self._db.captions(views=('name', 'id', 'location')))
+
+    @view('update.j2')
+    def _update(self, id):
+        if request.forms:
+            self._db.set_captions(id, request.forms)
+        captions = self._db.captions(filters={"id": id})
+        if len(captions) == 1:
+            return captions[0]
+        redirect("/")
 
     def main(self):
         run(self._app, host='0.0.0.0', port=self._config.http_port)
